e502dd5675de322cd41ff6a7d118ac8e832e5d82
Nguyen Thanh Nhan <nhan22020@gmail.com>
2023-02-21 09:47:49 +0700

    commit

---
+++ for1.java
@@ -0,0 +1,10 @@
+package bt2;
+
+public class for1{
+    public static void main(String agvs[]){
+        int i;
+        for(i = 1; i<=10;i++){
+            System.out.println(i);
+        }
+    }
+}
